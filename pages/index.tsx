f648d81d1b30079cc5e34c2079ba51d05def4ecf
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2020-06-05 01:26:47 -0400

    picked my pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,10 @@
-export default () =>
-    <div>jordan frankfurt lives here</div>
+import { css } from '@emotion/core'
+import Layout from 'components/Layout'
+
+const rootCss = css``
+
+export default () => (
+  <Layout>
+    <div css={rootCss}>index</div>
+  </Layout>
+)

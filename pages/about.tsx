5d396804de684b560033d439ec2dbc6e22264cbd
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2024-07-05 17:28:07 -0500

    about page update

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,53 @@
 import Layout from 'components/Layout'
+import Image from 'next/image'
 
 export default function About() {
   return (
     <Layout>
       <div className="min-w-[360px] w-[45vw]">
-        <h1>About me</h1>
-        <p>
-          I fight people (
+        <h2 className="text-3xl mb-2 mt-6">I fight people</h2>
+        <p className="ml-2">
+          (
           <a
             href="https://www.youtube.com/watch?v=4qdaVwwz1bw"
             target="_blank"
-            rel="noopener noreferrer"
-          >
-            poorly
+            rel="noopener noreferrer">
+            Poorly
+          </a>{' '}
+          and{' '}
+          <a href="https://smoothcomp.com/en/profile/335420">
+            as often as I can.
           </a>
-          ) as often as I can.
+          )
         </p>
+        <h2 className="text-3xl mb-2 mt-6">
+          I like to model & print myself little useful objects:
+        </h2>
+        <ul>
+          <li>
+            <Image
+              src="/bracket-render.png"
+              width={250}
+              height={250}
+              alt="wall hanger render"
+            />
+            <a href="/painting-hanger.3mf" download="/painting-hanger.3mf">
+              Heavy-duty wall hanger
+            </a>
+          </li>
+          <li>
+            <Image
+              src="/sharkbite-vinyl-fitting_render.png"
+              width={250}
+              height={250}
+              alt="sharkbite fitting render"
+            />
+            <a href="/sharkbite-fitting.3mf" download="/sharkbite-fitting.3mf">
+              Sharkbite fitting (to attach an orange juice bottle to vinyl
+              tubing)
+            </a>
+          </li>
+        </ul>
       </div>
     </Layout>
   )
